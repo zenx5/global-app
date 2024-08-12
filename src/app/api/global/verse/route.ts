2c873f1b6f58e9966759b5d1cd1c54e463188a01
Octavio Martinez <omartinez1618@gmail.com>
2024-08-11 22:43:37 -0400

    verse random

---
+++ src/app/api/global/verse/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+import bible from "./../../../../tools/es_rvr.json"
+
+export async function GET() {
+
+    const capther = Math.floor( Math.random()*bible[19].chapters.length )
+    const verse = Math.floor( Math.random()*bible[19].chapters[capther].length )
+
+    return NextResponse.json({
+        status: 200,
+        message: 'Verse fetched successfully',
+        data: {
+            quote: bible[19].chapters[capther][verse],
+            reference: `Proverbios ${capther + 1}:${verse + 1}`
+        },
+    });
+}
