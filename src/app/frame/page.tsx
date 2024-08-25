d5b48520158c15d83d532aea3465118038524e34
Octavio Martinez <omartinez1618@gmail.com>
2024-08-24 22:50:37 -0400

    iframe twitch

---
+++ src/app/frame/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+
+    return <iframe
+        src="https://player.twitch.tv/?channel=zenx5dev&parent=global-app-teal.vercel.app"
+        height="500"
+        width="500"
+        allowFullScreen
+    ></iframe>
+}
