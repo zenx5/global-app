0af05e831a5cdda4760f4ca34019cf01cd0b4483
Octavio Martinez <omartinez1618@gmail.com>
2024-09-15 01:21:19 -0400

    try catch

--- src/app/listen/page.tsx
+++ src/app/listen/page.tsx
@@ -2,10 +2,14 @@
 
 export default async function Page(){
 
-    const response = await fetch(`${process.env.LOCALHOST}/api/config`)
-    const { url } = await response.json()
+    try{
+        const response = await fetch(`${process.env.LOCALHOST}/api/config`)
+        const { url } = await response.json()
 
-    return <div className="flex items-center justify-center w-screen h-screen">
-        <audio src={url} autoPlay controls ></audio>
-    </div>
+        return <div className="flex items-center justify-center w-screen h-screen">
+            <audio src={url} autoPlay controls ></audio>
+        </div>
+    } catch (error) {
+        return <div>Error loading audio</div>
+    }
 }
